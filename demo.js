fc47ca4f38ea90278b893d94d690d05adb218aac
Forrest Oliphant <forrest@sembiki.com>
2017-02-11 15:56:50 -0500

    3 parts from real pattern, inches

--- demo.js
+++ demo.js
@@ -3,6 +3,9 @@ const startApp = require('./src/app')
 // Measurement keys from http://developer.bodylabs.com/red_api_measurements.html
 
 const pattern = {
+  metadata: {
+    system: 'us',
+  },
   measurements: {
     'neck_base': 200,
     'across_back_shoulder_neck_base_shoulder': 200,
@@ -16,19 +19,18 @@ const pattern = {
   },
   parts: [
     {
+      id: 'front',
+      symmetry: true,
       points: [
-        {x: 0, y: 0},
-        {x: 20, y: -10},
-        {x: 40, y: -10},
-        {x: 40, y: 10},
-        {x: 20, y: 10},
-        {x: 20, y: 60},
-        {x: 0, y: 60},
-        {x: -20, y: 60},
-        {x: -20, y: 10},
-        {x: -40, y: 10},
-        {x: -40, y: -10},
-        {x: -20, y: -10},
+        {x: 1.0, y: -1.0},
+        {x: 8.0, y: -1.7},
+        {x: 11.0, y: -3.5},
+        {x: 10.5, y: -11.0},
+        {x: 11.0, y: -15.0},
+        {x: 12.5, y: -19.5},
+        {x: 10.0, y: -21.0},
+        {x: 9.5, y: -25.2},
+        {x: 1.0, y: -27.0},
       ],
       distances: [
         // height
@@ -44,11 +46,39 @@ const pattern = {
       // ],
     },
     {
+      id: 'back',
+      symmetry: true,
       points: [
-        {x: -10, y: 0},
-        {x: 0, y: 10},
-        {x: 5, y: 0},
-        {x: 0, y: -10},
+        {x: 3.1, y: 1.0},
+        {x: 1.0, y: 4.75},
+        {x: 6.6, y: 9.6},
+        {x: 8.0, y: 10.0},
+        {x: 10.0, y: 11.0},
+        {x: 10.0, y: 12.75},
+        {x: 20.0, y: 11.5},
+        {x: 26.25, y: 11.5},
+        {x: 27.5, y: 6.0},
+        {x: 28.0, y: 1.0},
+      ],
+      distances: [
+      ],
+      angles: [
+      ],
+    },
+    {
+      id: 'arm front',
+      symmetry: false,
+      points: [
+        {x: 1.5, y: -1.0},
+        {x: 1.0, y: -3.0},
+        {x: 1.0, y: -6.1},
+        {x: 17.0, y: -5.0},
+        {x: 25.2, y: -3.5},
+        {x: 24.75, y: -1.0},
+        {x: 21.0, y: -1.5},
+        {x: 16.0, y: -1.25},
+        {x: 12.0, y: -1.5},
+        {x: 7.0, y: -2.0},
       ],
       distances: [
       ],
