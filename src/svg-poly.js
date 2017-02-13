265e14955f772548f943dfcb18f7e7a1c2d430ac
Forrest Oliphant <forrest@sembiki.com>
2017-02-13 14:46:35 -0500

    extract some point / svg utils

--- src/svg-poly.js
+++ src/svg-poly.js
@@ -1,70 +1,5 @@
 const html = require('choo/html')
-
-function minMaxBox (points) {
-  let top = -Infinity
-  let right = -Infinity
-  let bottom = Infinity
-  let left = Infinity
-  for (let i = 0, len = points.length; i < len; i++) {
-    const {x, y} = points[i]
-    top = Math.max(top, y)
-    right = Math.max(right, x)
-    bottom = Math.min(bottom, y)
-    left = Math.min(left, x)
-  }
-  const width = right - left
-  const height = top - bottom
-  return {left, bottom, width, height}
-}
-
-function reflectPoint (p, p0, p1) {
-  const dx = p1.x - p0.x
-  const dy = p1.y - p0.y
-  const a = (dx * dx - dy * dy) / (dx * dx + dy * dy)
-  const b = 2 * dx * dy / (dx * dx + dy * dy)
-  const x = a * (p.x - p0.x) + b * (p.y - p0.y) + p0.x
-  const y = b * (p.x - p0.x) - a * (p.y - p0.y) + p0.y
-
-  return {x, y}
-}
-
-function makeSymmetrical (points) {
-  const simPoints = points.slice()
-  const a = points[0]
-  const b = points[points.length-1]
-  let i = points.length - 1
-  while (i > 0) {
-    const point = points[i]
-    simPoints.push(reflectPoint(point, a, b))
-    i--
-  }
-  return simPoints
-}
-
-function dClosed (points) {
-  let path = 'M '
-  for (let i = 0, len = points.length; i < len; i++) {
-    const {x, y} = points[i]
-    path += x + ' ' + y + ' L '
-  }
-  path += points[0].x + ' ' + points[0].y
-  return path
-}
-
-function fit (points, fitWidth, fitHeight, padding = 4) {
-  fitWidth -= padding * 2
-  fitHeight -= padding * 2
-  const {left, bottom, width, height} = minMaxBox(points)
-  const scale = Math.min(fitWidth/width, fitHeight/height)
-  const paddingLeft = Math.max(0, (fitWidth - (width * scale)) / 2)
-  const paddingTop = Math.max(0, (fitHeight - (height * scale)) / 2)
-  return points.map(function (point) {
-    return {
-      x: ((point.x - left) * scale) + paddingLeft + padding,
-      y: ((point.y - bottom) * scale) + paddingTop + padding,
-    }
-  })
-}
+const {pointsFit, pointsWithSymmetry, dClosed} = require('./geometry.js')
 
 function line (a, b) {
   return html`<line
@@ -80,10 +15,9 @@ function line (a, b) {
 function svgPoly (points, symmetry = false, width = 72, height = 72) {
   const lastIndex = points.length-1
   if (symmetry) {
-    points = makeSymmetrical(points)
+    points = pointsWithSymmetry(points)
   }
-  points = fit(points, width, height)
-  const d = dClosed(points)
+  points = pointsFit(points, width, height)
   return html`
     <svg
       width="${width}" height="${height}"
@@ -95,7 +29,7 @@ function svgPoly (points, symmetry = false, width = 72, height = 72) {
       "
     >
       <path
-        d="${d}"
+        d="${dClosed(points)}"
         fill="#fff" stroke="black" stroke-width="1"
       />
       ${symmetry && line(points[0], points[lastIndex])}
